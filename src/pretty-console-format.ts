bba9477a6c9b4948eae176fbf36bf6b745bf5301
Tristan Menzel <tristanmenzel@gmail.com>
2022-09-01 15:48:44 +1000

    Configure log level colors so the test passes

--- src/pretty-console-format.ts
+++ src/pretty-console-format.ts
@@ -1,11 +1,11 @@
 import { Format, format, TransformableInfo } from 'logform'
 import yamlifyObject from 'yamlify-object'
 import yamlifyColors from 'yamlify-object-colors'
-import colors from 'colors'
+import colors from '@colors/colors'
 
 export const prettyConsoleFormat = (): Format =>
   format.combine(
-    format.colorize({ colors: { info: 'blue' } }),
+    format.colorize(),
     format.timestamp({ format: 'HH:mm:ss.SSS' }),
     format.printf((info: TransformableInfo) => {
       const { level, timestamp, message, ...meta } = info
